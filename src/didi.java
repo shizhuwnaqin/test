dc8d25a9864942fc47161bfb316c4eff4f1df5c6
zhu <867431215@qq.com>
2020-06-15 19:04:12 +0800

    初始化

--- src/didi.java
+++ src/didi.java
@@ -1,12 +1,7 @@
 public class didi {
     public static void main(String args[]){
 
-        System.out.print("1");
-        System.out.print("3");
-// //         System.out.print("4");
-//         System.out.print("2");
-//        System.out.print("4");
-//        System.out.print("2");
+
 
     }
 
