a3c979f95868bd46c530f1e376bbd6630e38b4cb
zhu <867431215@qq.com>
2020-06-16 14:25:52 +0800

    新增3

--- src/didi.java
+++ src/didi.java
@@ -2,8 +2,8 @@ public class didi {
     public static void main(String args[]){
 
         System.out.println("1");
-        System.out.println("2");
-
+//        System.out.println("2");
+        System.out.println("3");
     }
 
 }
