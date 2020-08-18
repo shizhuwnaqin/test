53ef73262a31c0fbb9f08a3bfa4a4be1974b1ddc
zhu <867431215@qq.com>
2020-08-18 13:55:23 +0800

    打tag2

--- src/didi.java
+++ src/didi.java
@@ -2,6 +2,7 @@ public class didi {
     public static void main(String args[]){
         System.out.println("初始化");
         System.out.println("tag1");
+        System.out.println("tag2");
     }
 
 }
