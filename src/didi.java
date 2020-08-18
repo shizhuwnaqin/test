8d725d210f5f7106eab064b16d6796795762c919
zhu <867431215@qq.com>
2020-08-18 18:39:02 +0800

    add v0.22

--- src/didi.java
+++ src/didi.java
@@ -1,7 +1,8 @@
 public class didi {
     public static void main(String args[]){
         System.out.println("初始化");
-        System.out.println("v0.2");
+        System.out.println("v0.22");
+
     }
 
 }
