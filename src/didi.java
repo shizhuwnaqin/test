c3c3ac68646eaf771a95f2409ac3a8ccb8702d7c
zhu <867431215@qq.com>
2020-08-18 15:18:59 +0800

    init

--- src/didi.java
+++ src/didi.java
@@ -1,8 +1,6 @@
 public class didi {
     public static void main(String args[]){
         System.out.println("初始化");
-        System.out.println("tag1");
-        System.out.println("tag2");
     }
 
 }
