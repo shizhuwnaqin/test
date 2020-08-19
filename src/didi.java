03acb3eaefe5d436a2a94838555d6718ce19e985
zhu <867431215@qq.com>
2020-08-19 14:46:35 +0800

    add feater-02

--- src/didi.java
+++ src/didi.java
@@ -2,6 +2,7 @@ public class didi {
     public static void main(String args[]){
         System.out.println("初始化");
         System.out.println("分支1");
+        System.out.println("分支2");
     }
 
 }
